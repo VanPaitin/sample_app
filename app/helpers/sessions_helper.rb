5255729a878ce460a1e3db450f518ce91f749b41
Mayowa Pitan <mayowa.pitan@andela.com>
2016-07-05 04:32:10 +0100

    chore (controllers): refactor code for better readability

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,7 +26,7 @@ module SessionsHelper
 
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
-    !current_user.nil?
+    current_user.present?
   end
 
   # Forgets a persistent session.
